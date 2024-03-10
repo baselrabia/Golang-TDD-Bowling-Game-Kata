522fb51f212b5e6b65611bc526c74f50231dac88
baselrabia <baselrabia@gmail.com>
2024-03-10 22:17:52 +0200

    the game should be 20 as the last 2 pouns is a frame will be accumealted ??

--- pkg/game/game.go
+++ pkg/game/game.go
@@ -18,7 +18,7 @@ func (g *Game) Roll(pins int) {
 func (g *Game) Score() int {
 	score := 0
 
-	for i := 0; i < len(g.rolled); i++ {
+	for i := 0; i < len(g.rolled)-2; i++ {
 
 		if (g.rolled[i] + g.rolled[i+1] == 10) { //spare 
 			score += g.rolled[i+2]
